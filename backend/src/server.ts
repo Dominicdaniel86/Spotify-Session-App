7700d9df9d95acb8748e2e3458f060b5f73e6ba1
Dominic Daniel Krämer <Dominicdaniel3107@gmail.com>
2024-11-04 23:28:54 +0100

    add cookie parser to nodejs and improve accessability

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+app.use(cookieParser());
 const PORT = 3000;
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __dirname = path.join(path.dirname(fileURLToPath(import.meta.url)), '..');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+    console.log(req.cookies);
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.listen(PORT, () => {
