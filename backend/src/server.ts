8db849d8e2402b927c2b3a092940b392c7ba2ae8
Dominic Daniel Krämer <Dominicdaniel3107@gmail.com>
2024-11-04 22:52:09 +0100

    Replace html pages to nodejs and implement basic config for nginx as a reverse proxy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const PORT = 3000;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 });
 
 app.listen(PORT, () => {
