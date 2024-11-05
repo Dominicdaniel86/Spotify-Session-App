77fb785c765c8f01457d99e13e935727cb18d348
Dominic Daniel Krämer <Dominicdaniel3107@gmail.com>
2024-11-05 12:24:16 +0100

    add first authentication step for spotify API

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,16 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import cookieParser from 'cookie-parser';
+import { requestUserAuthorization } from './spotify-authentication.js';
 
 const app = express();
 app.use(cookieParser());
-const PORT = 3000;
+
 const __dirname = path.join(path.dirname(fileURLToPath(import.meta.url)), '..');
 
+const PORT = process.env.PORT || 3000;
+
+// serve static files
 app.get('/', (req, res) => {
     console.log(req.cookies);
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
@@ -23,6 +27,18 @@ app.get('/playlist', (req, res) => {
 
 app.get('/admin', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'admin.html'));
+});
+
+// api calls
+app.get('/spotify/login', (req, res) => {
+    console.debug("debug: spotify login started");
+    requestUserAuthorization(res);
+});
+
+app.get('/spotify/callback', (req, res) => {
+    console.log("debug: spotify callback");
+    console.log("debug: received " + req.query.code);
+    res.redirect("/admin");
 });
 
 app.listen(PORT, () => {
