2d0c2169dd40ae8fbac03fe1d575f9485b23bd0c
Dominic Daniel Krämer <Dominicdaniel3107@gmail.com>
2024-12-12 05:13:10 +0100

    Add general http-request method to frontend

--- frontend/src/http-requests.ts
+++ frontend/src/http-requests.ts
@@ -6,6 +6,31 @@ declare global {
     }
 }
 
+async function httpRequest<T>(url: string, method: string, data?: object): Promise<T> {
+
+    const options: RequestInit = {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    if (data) {
+        options.body = JSON.stringify(data);
+    }
+
+    return fetch(url, options)
+    .then(response => {
+        if(!response.ok)
+            throw new Error("response was not ok: " + response.status);
+        console.log("response ok")
+        return response.json();
+    })
+    .catch(err => {
+        console.error("error: ", err);
+    })
+}
+
 window.put = function put(url: string, data: object) {
     fetch(url, {
         method: 'PUT',
